943a7a1e2582fbba64d450bcb887852815b81346
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-08-07 08:40:04 +0600

    configured toggler mode for dark moe

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes'
+import { ThemeProvider } from './providers/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <RouterProvider router={router}/>
+    </ThemeProvider>
   </StrictMode>,
 )
